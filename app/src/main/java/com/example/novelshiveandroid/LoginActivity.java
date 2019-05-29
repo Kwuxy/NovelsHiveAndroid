29e975c1ff640193374f49d286bfc2bfac21aeb5
NBarseyni <nicolas.barseyni@protonmail.com>
2019-05-29 22:24:45 +0200

    Add toolbar for title login activity

--- app/src/main/java/com/example/novelshiveandroid/LoginActivity.java
+++ app/src/main/java/com/example/novelshiveandroid/LoginActivity.java
@@ -2,6 +2,7 @@ package com.example.novelshiveandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +10,10 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Toolbar myToolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(myToolbar);
+
+        getSupportActionBar().setTitle(R.string.title_login_activity);
     }
 }

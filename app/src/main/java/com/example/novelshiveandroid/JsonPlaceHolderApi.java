85cab9393546adffd299ac800a5065d45fd2c85d
Nico C <nicococq25@gmail.com>
2019-05-29 22:34:34 +0200

    Add User Methods To Constitute A Profile
    Add getUserInfos for description
    Add getUserFavorites for favorites stories
    Add getUserStories for user's stories

--- app/src/main/java/com/example/novelshiveandroid/JsonPlaceHolderApi.java
+++ app/src/main/java/com/example/novelshiveandroid/JsonPlaceHolderApi.java
@@ -4,11 +4,21 @@ import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 
 public interface JsonPlaceHolderApi {
 
     @GET("users")
     Call<List<User>> getUsers();
+
+    @GET("users/{id}")
+    Call<User> getUserInfos(@Path("id") int userId);
+
+    @GET("users/{id}/favorites")
+    Call<List<Story>> getUserFavorites(@Path("id") int userId);
+
+    @GET("users/{id}/stories")
+    Call<List<Story>> getUserStories(@Path("id") int userId);
 
     @GET("stories")
     Call<List<Story>> getStories();

19aa1792b8578e1d2db019aa8c57bf64de588d38
NBarseyni <nicolas.barseyni@protonmail.com>
2019-05-31 22:35:09 +0200

    TEST go to home activity on sign in

--- app/src/main/java/com/example/novelshiveandroid/LoginActivity.java
+++ app/src/main/java/com/example/novelshiveandroid/LoginActivity.java
@@ -1,8 +1,10 @@
 package com.example.novelshiveandroid;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -15,5 +17,10 @@ public class LoginActivity extends AppCompatActivity {
         setSupportActionBar(myToolbar);
 
         getSupportActionBar().setTitle(R.string.title_login_activity);
+    }
+
+    public void goToHome(View view) {
+        Intent intent = new Intent(this, HomeActivity.class);
+        startActivity(intent);
     }
 }

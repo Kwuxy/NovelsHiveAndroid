40b58a367bf1fa1dc30686863c602786643a3cc8
NBarseyni <nicolas.barseyni@protonmail.com>
2019-06-01 22:38:18 +0200

    Add toolbar on settings activity

--- app/src/main/java/com/example/novelshiveandroid/SettingsActivity.java
+++ app/src/main/java/com/example/novelshiveandroid/SettingsActivity.java
@@ -2,12 +2,30 @@ package com.example.novelshiveandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 public class SettingsActivity extends AppCompatActivity {
+
+    private Toolbar myToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        this.configureToolbar();
+    }
+
+    private void configureToolbar() {
+        myToolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(myToolbar);
+        getSupportActionBar().setTitle("Settings");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        finish();
+        return true;
     }
 }

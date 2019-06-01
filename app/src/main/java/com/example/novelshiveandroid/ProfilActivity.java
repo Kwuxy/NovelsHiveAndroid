2193499dbbc4b51dabdf16570ad7adbc79eca495
NBarseyni <nicolas.barseyni@protonmail.com>
2019-06-01 22:37:44 +0200

    Add toolbar on ProfilActivity

--- app/src/main/java/com/example/novelshiveandroid/ProfilActivity.java
+++ app/src/main/java/com/example/novelshiveandroid/ProfilActivity.java
@@ -2,12 +2,30 @@ package com.example.novelshiveandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 public class ProfilActivity extends AppCompatActivity {
+
+    private Toolbar myToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profil);
+
+        this.configureToolbar();
+    }
+
+    private void configureToolbar() {
+        myToolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(myToolbar);
+        getSupportActionBar().setTitle("Profile");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        finish();
+        return true;
     }
 }

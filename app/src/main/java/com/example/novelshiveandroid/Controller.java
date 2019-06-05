faf124992883bd774b774f0095f1f0fa965a84fd
Nico C <nicococq25@gmail.com>
2019-06-05 17:14:00 +0200

    Add Get...() Methods for Story Searching Filters

--- app/src/main/java/com/example/novelshiveandroid/Controller.java
+++ app/src/main/java/com/example/novelshiveandroid/Controller.java
@@ -1,6 +1,12 @@
 package com.example.novelshiveandroid;
 
+import com.example.novelshiveandroid.models.Kind;
+import com.example.novelshiveandroid.models.Language;
+import com.example.novelshiveandroid.models.Rating;
+import com.example.novelshiveandroid.models.Status;
 import com.example.novelshiveandroid.models.Story;
+import com.example.novelshiveandroid.models.Tag;
+import com.example.novelshiveandroid.models.Universe;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -25,6 +31,7 @@ public abstract class Controller {
 
     private static JsonPlaceHolderApi jsonPlaceHolderApi = retrofit.create(JsonPlaceHolderApi.class);
 
+    //Get Stories With Searching Filters Parameters
     public static List<Story> getStories(Map<String, String> parameters){
         final List<Story> stories = new ArrayList<>();
         Call<List<Story>> call = jsonPlaceHolderApi.getStories(parameters);
@@ -49,4 +56,155 @@ public abstract class Controller {
         return stories;
 
     }
+
+    //Get All Tags For Story Searching Filters
+    public static List<Tag> getTags(){
+        final List<Tag> tags = new ArrayList<>();
+        Call<List<Tag>> call = jsonPlaceHolderApi.getTags();
+        call.enqueue(new Callback<List<Tag>>() {
+            @Override
+            public void onResponse(Call<List<Tag>> call, Response<List<Tag>> response) {
+                if (!response.isSuccessful()) {
+                    System.out.print("Code : " + response.code());
+                    return;
+                }
+
+                tags.clear();
+                tags.addAll(response.body());
+            }
+
+            @Override
+            public void onFailure(Call<List<Tag>> call, Throwable t) {
+                System.out.print(t.getMessage());
+            }
+        });
+
+        return tags;
+    }
+
+    //Get All Kinds For Story Searching Filters
+    public static List<Kind> getKinds(){
+        final List<Kind> kinds = new ArrayList<>();
+        Call<List<Kind>> call = jsonPlaceHolderApi.getKinds();
+        call.enqueue(new Callback<List<Kind>>() {
+            @Override
+            public void onResponse(Call<List<Kind>> call, Response<List<Kind>> response) {
+                if (!response.isSuccessful()) {
+                    System.out.print("Code : " + response.code());
+                    return;
+                }
+
+                kinds.clear();
+                kinds.addAll(response.body());
+            }
+
+            @Override
+            public void onFailure(Call<List<Kind>> call, Throwable t) {
+                System.out.print(t.getMessage());
+            }
+        });
+
+        return kinds;
+    }
+
+    //Get All Ratings For Story Searching Filters
+    public static List<Rating> getRatings(){
+        final List<Rating> ratings = new ArrayList<>();
+        Call<List<Rating>> call = jsonPlaceHolderApi.getRatings();
+        call.enqueue(new Callback<List<Rating>>() {
+            @Override
+            public void onResponse(Call<List<Rating>> call, Response<List<Rating>> response) {
+                if (!response.isSuccessful()) {
+                    System.out.print("Code : " + response.code());
+                    return;
+                }
+
+                ratings.clear();
+                ratings.addAll(response.body());
+            }
+
+            @Override
+            public void onFailure(Call<List<Rating>> call, Throwable t) {
+                System.out.print(t.getMessage());
+            }
+        });
+
+        return ratings;
+    }
+
+    //Get All Universes For Story Searching Filters
+    public static List<Universe> getUniverses(){
+        final List<Universe> universes = new ArrayList<>();
+        Call<List<Universe>> call = jsonPlaceHolderApi.getUniverses();
+        call.enqueue(new Callback<List<Universe>>() {
+            @Override
+            public void onResponse(Call<List<Universe>> call, Response<List<Universe>> response) {
+                if (!response.isSuccessful()) {
+                    System.out.print("Code : " + response.code());
+                    return;
+                }
+
+                universes.clear();
+                universes.addAll(response.body());
+            }
+
+            @Override
+            public void onFailure(Call<List<Universe>> call, Throwable t) {
+                System.out.print(t.getMessage());
+            }
+        });
+
+        return universes;
+    }
+
+    //Get All Languages For Story Searching Filters
+    public static List<Language> getLanguages(){
+        final List<Language> languages = new ArrayList<>();
+        Call<List<Language>> call = jsonPlaceHolderApi.getLanguages();
+        call.enqueue(new Callback<List<Language>>() {
+            @Override
+            public void onResponse(Call<List<Language>> call, Response<List<Language>> response) {
+                if (!response.isSuccessful()) {
+                    System.out.print("Code : " + response.code());
+                    return;
+                }
+
+                languages.clear();
+                languages.addAll(response.body());
+            }
+
+            @Override
+            public void onFailure(Call<List<Language>> call, Throwable t) {
+                System.out.print(t.getMessage());
+            }
+        });
+
+        return languages;
+    }
+
+    //Get All Status For Story Searching Filters
+    public static List<Status> getStatus(){
+        final List<Status> status = new ArrayList<>();
+        Call<List<Status>> call = jsonPlaceHolderApi.getStatus();
+        call.enqueue(new Callback<List<Status>>() {
+            @Override
+            public void onResponse(Call<List<Status>> call, Response<List<Status>> response) {
+                if (!response.isSuccessful()) {
+                    System.out.print("Code : " + response.code());
+                    return;
+                }
+
+                status.clear();
+                status.addAll(response.body());
+            }
+
+            @Override
+            public void onFailure(Call<List<Status>> call, Throwable t) {
+                System.out.print(t.getMessage());
+            }
+        });
+
+        return status;
+    }
+    
 }

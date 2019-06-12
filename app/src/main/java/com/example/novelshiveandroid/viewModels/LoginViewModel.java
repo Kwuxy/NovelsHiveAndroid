bf0ecfad46053b75c53b7a401e6b753e8950a3a5
Nico C <nicococq25@gmail.com>
2019-06-13 00:40:33 +0200

    Add Login View, Presenter and ViewModel To Use LoginUser Funct

---
+++ app/src/main/java/com/example/novelshiveandroid/viewModels/LoginViewModel.java
@@ -0,0 +1,57 @@
+package com.example.novelshiveandroid.viewModels;
+
+import android.text.TextUtils;
+
+import com.example.novelshiveandroid.Globals;
+import com.example.novelshiveandroid.models.Token;
+import com.example.novelshiveandroid.presenters.LoginPresenter;
+import com.example.novelshiveandroid.views.LoginView;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+import static com.example.novelshiveandroid.APIClient.jsonPlaceHolderApi;
+
+public class LoginViewModel implements LoginPresenter {
+
+    LoginView mLoginView;
+
+    public LoginViewModel(LoginView loginView){
+        this.mLoginView = loginView;
+    }
+
+    @Override
+    public void performLogin(String email, String password) {
+
+        if(TextUtils.isEmpty(email) || TextUtils.isEmpty(password)){
+            mLoginView.loginValidations();
+        }
+        else {
+            Call<Token> call = jsonPlaceHolderApi.loginUser(email, password);
+
+            call.enqueue(new Callback<Token>() {
+                @Override
+                public void onResponse(Call<Token> call, Response<Token> response) {
+                    if(!response.isSuccessful()){
+                        mLoginView.loginUnsuccessful();
+                        return;
+                    }
+                    setTokenOnGlobals(response.body());
+                    mLoginView.loginSuccess();
+                }
+
+                @Override
+                public void onFailure(Call<Token> call, Throwable t) {
+                    System.out.print(t.getMessage());
+                    mLoginView.loginFailure();
+                }
+            });
+        }
+    }
+
+    @Override
+    public void setTokenOnGlobals(Token token) {
+        Globals.getInstance().setCurrentToken(token);
+    }
+}

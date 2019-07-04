0d3fbe6cfd380b97799831c434616158106a3ceb
NBarseyni <nicolas.barseyni@protonmail.com>
2019-07-04 21:54:50 +0200

    Add basic chapter comments activity

---
+++ app/src/main/java/com/example/novelshiveandroid/activities/ChapterCommentsActivity.java
@@ -0,0 +1,15 @@
+package com.example.novelshiveandroid.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.novelshiveandroid.R;
+
+public class ChapterCommentsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chapter_comments);
+    }
+}

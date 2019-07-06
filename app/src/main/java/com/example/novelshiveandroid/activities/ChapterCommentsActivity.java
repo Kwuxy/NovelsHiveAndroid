fcccd17581967dcd426088b37364f35ee4c49ce8
NBarseyni <nicolas.barseyni@protonmail.com>
2019-07-06 22:24:22 +0200

    Add content to ChapterCommentsActivity
    - Add ChapterCommentsAdapter
    - Add item chapter comment
    - strings resources
    - Add recyclerView to Activity

--- app/src/main/java/com/example/novelshiveandroid/activities/ChapterCommentsActivity.java
+++ app/src/main/java/com/example/novelshiveandroid/activities/ChapterCommentsActivity.java
@@ -1,15 +1,28 @@
 package com.example.novelshiveandroid.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
 import com.example.novelshiveandroid.R;
+import com.example.novelshiveandroid.adapter.ChapterCommentsAdapter;
 
-public class ChapterCommentsActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+import static com.example.novelshiveandroid.Globals.KEY_CHAPTER_ID;
+
+public class ChapterCommentsActivity extends AppCompatActivity implements ChapterCommentsView {
 
     private Toolbar myToolbar;
+
+    private RecyclerView rvChapterCommments;
+    private ChapterCommentsAdapter chapterCommentsAdapter;
+    private List<PublishedComment> chapterComments;
+
+    private ChapterCommentsPresenter mChapterCommentsPresenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +30,14 @@ public class ChapterCommentsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_chapter_comments);
 
         this.configureToolbar();
+        this.initUI();
+
+        Intent intent = getIntent();
+        int chapterId = intent.getIntExtra(KEY_CHAPTER_ID, 0);
+
+        mChapterCommentsPresenter = new ChapterCommentsViewModel(this);
+        // Request data
+        mChapterCommentsPresenter.getChapterComments(chapterId);
     }
 
     private void configureToolbar() {
@@ -26,9 +47,25 @@ public class ChapterCommentsActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
+    private void initUI() {
+        rvChapterCommments = findViewById(R.id.rv_chapter_comments);
+        chapterComments = new ArrayList<>();
+        chapterCommentsAdapter = new ChapterCommentsAdapter(chapterComments);
+        rvChapterCommments.setAdapter(chapterCommentsAdapter);
+    }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         finish();
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void displayChapterComments(List<PublishedComment> chapterComments) {
+        if (!chapterComments.isEmpty()) {
+            this.chapterComments.clear();
+            this.chapterComments.addAll(chapterComments);
+            chapterCommentsAdapter.notifyDataSetChanged();
+        }
     }
 }
---
+++ app/src/main/java/com/example/novelshiveandroid/adapter/ChapterCommentsAdapter.java
@@ -0,0 +1,61 @@
+package com.example.novelshiveandroid.adapter;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.novelshiveandroid.Globals;
+import com.example.novelshiveandroid.R;
+
+import java.util.ArrayList;
+
+public class ChapterCommentsAdapter extends RecyclerView.Adapter<ChapterCommentsAdapter.MyViewHolder> {
+
+        private List<PublishedComment> chapterComments;
+
+        public ChapterCommentsAdapter(List<PublishedComment> chapterComments) {
+            this.chapterComments = chapterComments;
+        }
+
+    @NonNull
+    @Override
+    public ChapterCommentsAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        Context context = viewGroup.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View chapterCommentView = inflater.inflate(R.layout.item_chapter_comment, viewGroup, false);
+
+        return new MyViewHolder(chapterCommentView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ChapterCommentsAdapter.MyViewHolder myViewHolder, int i) {
+        PublishedComment comment = chapterComments.get(i);
+        TextView tvUserName = myViewHolder.tvUserName;
+        tvUserName.setText(comment.getUsername());
+
+        TextView tvTextComment = myViewHolder.tvTextComment;
+        //tvTextComment.setText(comment.getText());
+        tvTextComment.setText(Globals.convertToText((ArrayList<Double>)comment.getText().get("data")));
+    }
+
+    @Override
+    public int getItemCount() {
+        return chapterComments.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView tvUserName;
+        public TextView tvTextComment;
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+            tvUserName = itemView.findViewById(R.id.tv_chapter_comment_user);
+            tvTextComment = itemView.findViewById(R.id.tv_chapter_comment_text);
+        }
+    }
+}

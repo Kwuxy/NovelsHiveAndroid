992011fbe09f9ba9eb2404f9f73b3c863f486a91
NBarseyni <nicolas.barseyni@protonmail.com>
2019-07-08 21:06:42 +0200

    Add username display to profileActivity

--- app/src/main/java/com/example/novelshiveandroid/activities/ProfileActivity.java
+++ app/src/main/java/com/example/novelshiveandroid/activities/ProfileActivity.java
@@ -4,7 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.widget.TextView;
 
+import com.example.novelshiveandroid.Globals;
 import com.example.novelshiveandroid.R;
 import com.example.novelshiveandroid.models.User;
 import com.example.novelshiveandroid.presenters.ProfilePresenter;
@@ -14,6 +16,7 @@ import com.example.novelshiveandroid.views.ProfileView;
 public class ProfileActivity extends AppCompatActivity implements ProfileView {
 
     private Toolbar myToolbar;
+    private TextView tvUsername;
 
     ProfilePresenter mProfilePresenter;
 
@@ -23,8 +26,11 @@ public class ProfileActivity extends AppCompatActivity implements ProfileView {
         setContentView(R.layout.activity_profile);
 
         this.configureToolbar();
+        this.initUI();
 
+        int userId = Globals.getInstance().getCurrentToken().getUserId();
         mProfilePresenter = new ProfileViewModel(ProfileActivity.this);
+        mProfilePresenter.getUserInfos(userId);
     }
 
     private void configureToolbar() {
@@ -34,6 +40,10 @@ public class ProfileActivity extends AppCompatActivity implements ProfileView {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
+    private void initUI() {
+        this.tvUsername = findViewById(R.id.tv_username);
+    }
+
     public boolean onOptionsItemSelected(MenuItem item) {
         finish();
         return true;
@@ -41,6 +51,6 @@ public class ProfileActivity extends AppCompatActivity implements ProfileView {
 
     @Override
     public void displayUserProfile(User user) {
-
+        this.tvUsername.setText(user.getUsername());
     }
 }

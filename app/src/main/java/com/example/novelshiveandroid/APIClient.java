108d04c3c4aab659dce2c5342e86b67b543e3168
Nico C <nicococq25@gmail.com>
2019-07-22 21:43:06 +0200

    Change API Link

--- app/src/main/java/com/example/novelshiveandroid/APIClient.java
+++ app/src/main/java/com/example/novelshiveandroid/APIClient.java
@@ -4,9 +4,8 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public abstract class APIClient {
-
-    // TODO: Change BASE_URL value for prod
-    private static String BASE_URL = "http://192.168.0.38:3000/api/";
+    
+    private static String BASE_URL = "http://ecs-first-run-alb-113054812.eu-west-3.elb.amazonaws.com:3000/api/";
 
     private static Retrofit retrofit = new Retrofit.Builder()
             .baseUrl(BASE_URL)
